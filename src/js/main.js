6b8baa96e9e9ef62b9bb5939ff03531633e13f78
dmitry.darevsky <darevsky.dmitry@gmail.com>
2019-12-05 12:14:12 +0300

    change margin in .main and width in .result

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,26 @@
 'use strict';
 
 let startBtn = document.getElementById('start'),
-  budgetValue = document.getElementsByClassName('budget-value')[0],
-  levelValue = document.getElementsByClassName('level-value')[0],
-  expensesValue = document.getElementsByClassName('expenses-value')[0],
-  optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
-  incomeValue = document.getElementsByClassName('income-value')[0],
-  monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
-  yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+	budgetValue = document.getElementsByClassName('budget-value')[0],
+	levelValue = document.getElementsByClassName('level-value')[0],
+	expensesValue = document.getElementsByClassName('expenses-value')[0],
+	optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+	incomeValue = document.getElementsByClassName('income-value')[0],
+	monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+	yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0],
 
-  expensesItem = document.getElementsByClassName('expenses-item'),
+	expensesItem = document.getElementsByClassName('expenses-item'),
 
-  expensesItemBtn = document.getElementsByTagName('button')[0],
-  optionalExpensesBtn = document.getElementsByTagName('button')[1],
-  countBudgetBtn = document.getElementsByTagName('button')[2],
+	expensesItemBtn = document.getElementsByTagName('button')[0],
+	optionalExpensesBtn = document.getElementsByTagName('button')[1],
+	countBudgetBtn = document.getElementsByTagName('button')[2],
 
-  optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+	optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
 
-  chooseIncome = document.querySelector('.choose-income'),
-  checkSavings = document.querySelector('#savings'),
-  chooseSum = document.querySelector('.choose-sum'),
-  choosePercent = document.querySelector('.choose-percent'),
-  yearValue = document.querySelector('.year-value'),
-  monthValue = document.querySelector('.month-value'),
-  dayValue = document.querySelector('.day-value');
+	chooseIncome = document.querySelector('.choose-income'),
+	checkSavings = document.querySelector('#savings'),
+	chooseSum = document.querySelector('.choose-sum'),
+	choosePercent = document.querySelector('.choose-percent'),
+	yearValue = document.querySelector('.year-value'),
+	monthValue = document.querySelector('.month-value'),
+	dayValue = document.querySelector('.day-value');
